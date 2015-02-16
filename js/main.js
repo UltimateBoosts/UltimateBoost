ea4a9779ee0b062c4dacc36783bfd475779c4a18
Cristian Andres Arrieta <caarrieta40@misena.edu.co>
2015-02-15 19:31:20 -0500

    Scroll Animation

--- js/main.js
+++ js/main.js
@@ -3,4 +3,39 @@ $(function ()
 		$(".aniServicios").hover(function(){
 			$(this).find("p").show("slide");
 		},function(){$(this).find("p").hide("slide");});
+
+		
+		var altoPantalla =screen.height;
+		var altoMenu=altoPantalla-125;
+		var calculoAltoServicios = (altoPantalla*53)/100;
+		var altoServicios=calculoAltoServicios+altoPantalla-600;
+	
+		
+		$(window).scroll(function() {
+  		
+  		
+	  		var scrolltop =$(this).scrollTop();
+	  		
+	  		if(scrolltop > altoServicios){
+	  			
+	  		$(".logo img").animate({ width:180, height:180 },"fast");	
+
+  			}
+  			if(scrolltop < altoServicios){
+	  			
+	  			$(".logo img").animate({ width:160, height:160 },"fast");
+
+  			}
+  			if(scrolltop > altoMenu){
+
+  				$(".Fixed").show("slow");
+
+  			}
+  			if(scrolltop < altoMenu){
+
+  				$(".Fixed").hide("slow");
+
+  			}
+  		
+  		})
 	});
